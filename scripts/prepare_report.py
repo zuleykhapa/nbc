27fcf29ddf36c18f4823533a960ed17ea0ae7ef7
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-12-16 13:44:40 +0100

    prepare repost from issue_ext files

--- scripts/prepare_report.py
+++ scripts/prepare_report.py
@@ -4,24 +4,25 @@ import pandas
 import tabulate
 import sys
 
-# parser = argparse.ArgumentParser()
-# parser.add_argument("file_name")
-# parser.add_argument("--platform")
-# parser.add_argument("--url")
-# args = parser.parse_args()
-# if not args:
-#     print("SET file_name argument")
+parser = argparse.ArgumentParser()
+parser.add_argument("input_csv")
+parser.add_argument("--platform")
+parser.add_argument("--url")
+args = parser.parse_args()
+if not args:
+    print("SET input_csv argument")
 
-# file_name = args.file_name
-# platform = args.platform
-# url = args.url
+input_csv = args.input_csv
+platform = args.platform
+url = args.url
 
-def report(file_name, platform, url):
+# def prepare_report(input_csv, platform, url):
+def prepare_report():
     with open("res_{}.md".format(platform), 'w') as f:
         f.write(f"\n\n#### Extensions failed to INSTALL\n")
         f.write(duckdb.query(f"""
                     SELECT architecture, version, extension
-                    FROM read_csv("{ file_name }")
+                    FROM read_csv("{ input_csv }")
                     WHERE failed_statement = 'INSTALL' 
                     ORDER BY nightly_build, architecture, runs_on, version, extension, failed_statement
                     """).to_df().to_markdown(index=False)
@@ -29,7 +30,7 @@ def report(file_name, platform, url):
         f.write(f"\n\n#### Extensions failed to LOAD\n")
         f.write(duckdb.query(f"""
                     SELECT architecture, version, extension
-                    FROM read_csv("{ file_name }")
+                    FROM read_csv("{ input_csv }")
                     WHERE failed_statement = 'LOAD' 
                     ORDER BY nightly_build, architecture, runs_on, version, extension, failed_statement
                     """).to_df().to_markdown(index=False)
--- scripts/verify_and_test.py
+++ scripts/verify_and_test.py
@@ -6,9 +6,9 @@ import sys
 import re
 import random
 import os
-# import string
 import subprocess
 import docker
+import prepare_report
 
 parser = argparse.ArgumentParser()
 # parser.add_argument("file_name")
@@ -39,83 +39,125 @@ def list_extensions(config) :
     matches = re.findall(pattern, content)
     return matches
 
-# VERIFY VERSION
-# repo = "duckdb/duckdb"
-
-if architecture.count("aarch64"):
-    name=architecture
-else:
-    name=platform
-
-# gh_command = [
-#     "gh", "run", "download",
-#     run_id,
-#     "--repo", repo,
-#     "--name", f"duckdb-binaries-{ name }"
-# ]
-# result = subprocess.run(gh_command, check=True, text=True, capture_output=True)
-# # subprocess.run()
-# print(result)
-
-# TEST ISTANLLING AND LOADING EXTENSIONS
 if nightly_build == 'Windows':
     duckdb='./duckdb.exe'
 else:
     duckdb='./duckdb'
+repo = "duckdb/duckdb"
 
-action=["INSTALL", "LOAD"]
-extensions=list_extensions(config)
-print(extensions)
-file_name = "issue_ext_{}.txt".format(nightly_build)
-counter = 0
-
-for ext in extensions:
+def verify_version():
     if architecture.count("aarch64"):
-        select_installed = [
+        name=architecture
+    else:
+        name=platform
+
+    gh_headSha_command = [
+        "gh", "run", "view",
+        run_id,
+        "--repo", repo,
+        "--json", "headSha",
+        "-q", ".headSha"
+    ]
+    full_sha = subprocess.run(gh_headSha_command, check=True, text=True, capture_output=True).stdout.strip()
+    if architecture.count("aarch64"):
+        pragma_version = [
             "docker", "run", "--rm", "--platform", "linux/aarch64",
             "-v", f"{ os.getcwd() }/duckdb:/duckdb",
-            "-e", f"ext={ ext }"
+            "-e", f"full_sha={ full_sha }"
             "ubuntu:22.04", "/bin/bash", "-c", 
-            f"./duckdb -c 'SELECT installed FROM duckdb_extensions() WHERE extension_name={ ext }';"
+            f"./duckdb --version"
         ]
     else:
-        select_installed = [
-            duckdb,
-            "-csv",
-            "-noheader",
-            "-c",
-            f"SELECT installed FROM duckdb_extensions() WHERE extension_name='{ ext }';"
+        pragma_version = [
+            duckdb, "--version"
         ]
-    result=subprocess.run(select_installed, check=True, text=True, capture_output=True)
-    is_installed = result.stdout.strip()
-    if is_installed == 'false':
-        for act in action:
-            print(f"{ act }ing { ext }...")
-            if architecture.count("aarch64"):
-                install_ext = [
-                    "docker", "run", "--rm", "--platform", "linux/aarch64",
-                    "-v", f"{ os.getcwd() }/duckdb:/duckdb",
-                    "-e", f"ext={ ext }"
-                    "ubuntu:22.04", "/bin/bash", "-c", f"./duckdb -c '{ act } { ext };'"
-                ]
-            else:
-                install_ext = [
-                    duckdb,
-                    "-c",
-                    f"{ act } '{ ext }';"
-                ]
-            try:
-                subprocess.run(install_ext, check=True, text=True, capture_output=True)
+    short_sha = subprocess.run(pragma_version, check=True, text=True, capture_output=True).stdout.strip().split()[-1]
+    if not full_sha.startswith(short_sha):
+        print(f"- The version of { nightly_build} build ({ short_sha }) doesn't match to the version triggered the build ({ full_sha }).\n")
+        with open("res_{}.md".format(platform), 'w') as f:
+            f.write(f"- The version of { nightly_build} build ({ short_sha }) doesn't match to the version triggered the build ({ full_sha }).\n")
+        return
 
-                print(act, ext, ": ", is_installed)
-            except subprocess.CalledProcessError as e:
-                with open(file_name, 'a') as f:
-                    if counter == 0:
-                        f.write(f"nightly_build,architecture,runs_on,version,extension,failed_statement\n")
-                        counter += 1
-                    f.write(f"{nightly_build },{ architecture },{ runs_on },,{ ext },{ act }\n")
-                print(f"Error running command for extesion { ext }: { e }")
-                print(f"stderr: { e.stderr }")
+def test_extensions():
+    action=["INSTALL", "LOAD"]
+    extensions=list_extensions(config)
+    print(extensions)
+    file_name = "issue_ext_{}.txt".format(nightly_build)
+    counter = 0
+
+    for ext in extensions:
+        if architecture.count("aarch64"):
+            select_installed = [
+                "docker", "run", "--rm", "--platform", "linux/aarch64",
+                "-v", f"{ os.getcwd() }/duckdb:/duckdb",
+                "-e", f"ext={ ext }"
+                "ubuntu:22.04", "/bin/bash", "-c", 
+                f"./duckdb -c 'SELECT installed FROM duckdb_extensions() WHERE extension_name={ ext }';"
+            ]
+        else:
+            select_installed = [
+                duckdb,
+                "-csv",
+                "-noheader",
+                "-c",
+                f"SELECT installed FROM duckdb_extensions() WHERE extension_name='{ ext }';"
+            ]
+        result=subprocess.run(select_installed, check=True, text=True, capture_output=True)
+        is_installed = result.stdout.strip()
+        if is_installed == 'false':
+            for act in action:
+                print(f"{ act }ing { ext }...")
+                if architecture.count("aarch64"):
+                    install_ext = [
+                        "docker", "run", "--rm", "--platform", "linux/aarch64",
+                        "-v", f"{ os.getcwd() }/duckdb:/duckdb",
+                        "-e", f"ext={ ext }"
+                        "ubuntu:22.04", "/bin/bash", "-c", f"./duckdb -c '{ act } { ext };'"
+                    ]
+                else:
+                    install_ext = [
+                        duckdb,
+                        "-c",
+                        f"{ act } '{ ext }';"
+                    ]
+                try:
+                    subprocess.run(install_ext, check=True, text=True, capture_output=True)
+
+                    print(act, ext, ": ", is_installed)
+                except subprocess.CalledProcessError as e:
+                    with open(file_name, 'a') as f:
+                        if counter == 0:
+                            f.write(f"nightly_build,architecture,runs_on,version,extension,failed_statement\n")
+                            counter += 1
+                        f.write(f"{nightly_build },{ architecture },{ runs_on },,{ ext },{ act }\n")
+                    print(f"Error running command for extesion { ext }: { e }")
+                    print(f"stderr: { e.stderr }")
+
 # create a Markdown report file
-import prepare_report
-prepare_report.report(file_name, platform, "url")
+# def prepare_report(file_name, platform, url):
+#     with open("res_{}.md".format(platform), 'w') as f:
+#         f.write(f"\n\n#### Extensions failed to INSTALL\n")
+#         f.write(duckdb.query(f"""
+#                     SELECT architecture, version, extension
+#                     FROM read_csv("{ file_name }")
+#                     WHERE failed_statement = 'INSTALL' 
+#                     ORDER BY nightly_build, architecture, runs_on, version, extension, failed_statement
+#                     """).to_df().to_markdown(index=False)
+#         )
+#         f.write(f"\n\n#### Extensions failed to LOAD\n")
+#         f.write(duckdb.query(f"""
+#                     SELECT architecture, version, extension
+#                     FROM read_csv("{ file_name }")
+#                     WHERE failed_statement = 'LOAD' 
+#                     ORDER BY nightly_build, architecture, runs_on, version, extension, failed_statement
+#                     """).to_df().to_markdown(index=False)
+#         )
+# prepare_report.report(file_name, platform, "url")
+
+def main():
+    verify_version()
+    test_extensions()
+    # prepare_report(file_name, platform, "url")
+
+if __name__ == "__main__":
+    main()
