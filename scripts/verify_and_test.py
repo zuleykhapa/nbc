dbe76447bada73dc83e854fea14c92a990244eb0
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-12-16 17:17:55 +0100

    .github/workflows/CallingNightlyBuildsCheck.yml

--- scripts/verify_and_test.py
+++ scripts/verify_and_test.py
@@ -124,6 +124,7 @@ def test_extensions(duckdb, name):
                     print(f"stderr: { e.stderr }")
 
 def main():
+    print(duckdb_path, "🦑")
     ls_duckdb_path = os.listdir(duckdb_path)
     if ls_duckdb_path:
         duckdb = f"{ duckdb_path }/{ ls_duckdb_path[0] }"
