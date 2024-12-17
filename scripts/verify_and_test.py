f1957e79ee241d36532df099c914cf12b7587835
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-12-17 17:29:05 +0100

    scripts/verify_and_test.py

--- scripts/verify_and_test.py
+++ scripts/verify_and_test.py
@@ -79,7 +79,7 @@ def test_extensions(tested_binary):
                 "-e", f"ext={ ext }",
                 "ubuntu:22.04",
                 "/bin/bash", "-c", 
-                f"/duckdb -c 'SELECT installed FROM duckdb_extensions() WHERE extension_name={ ext }';"
+                f"/duckdb -c 'SELECT installed FROM duckdb_extensions() WHERE extension_name='{ ext }';"
             ]
         else:
             select_installed = [
@@ -92,7 +92,7 @@ def test_extensions(tested_binary):
         result=subprocess.run(select_installed, check=True, text=True, capture_output=True)
         print(result.stdout)
         print(result.stderr)
-        
+
         is_installed = result.stdout.strip()
         if is_installed == 'false':
             for act in action:
@@ -103,7 +103,7 @@ def test_extensions(tested_binary):
                         "-v", f"{ tested_binary }:/duckdb",
                         "-e", f"ext={ ext }",
                         "ubuntu:22.04",
-                        "/bin/bash", "-c", f"/duckdb -c '{ act } { ext };'"
+                        "/bin/bash", "-c", f"/duckdb -c '{ act } { ext }';"
                     ]
                 else:
                     install_ext = [
