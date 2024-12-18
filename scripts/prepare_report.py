439391c761f0bda29f1f80f3f3c36dc53eb72dcd
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2024-12-18 13:23:29 +0100

    .github/workflows/CreateReport.yml

--- scripts/prepare_report.py
+++ scripts/prepare_report.py
@@ -7,18 +7,16 @@ import sys
 parser = argparse.ArgumentParser()
 parser.add_argument("input_csv")
 parser.add_argument("--platform")
-parser.add_argument("--url")
 args = parser.parse_args()
 if not args:
     print("SET input_csv argument")
 
 input_csv = args.input_csv
 platform = args.platform
-url = args.url
 
 # def prepare_report(input_csv, platform, url):
 def prepare_report():
-    with open("res_{}.md".format(platform), 'w') as f:
+    with open("failed_extensions{}.md".format(platform), 'w') as f:
         f.write(f"\n\n#### Extensions failed to INSTALL\n")
         f.write(duckdb.query(f"""
                     SELECT architecture, version, extension
