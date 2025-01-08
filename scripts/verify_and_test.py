7c716da15c93e5712413653e83f8021b86101999
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-01-08 15:29:58 +0100

    scripts/verify_and_test.py

--- scripts/verify_and_test.py
+++ scripts/verify_and_test.py
@@ -144,9 +144,9 @@ def verify_python_build_and_test_extensions(client, version, full_sha, file_name
     print(f"VERIFYING BUILD SHA FOR python{ version }")
     try:
         print("🪸🪸🪸")
-        container.exec_run("pip install -v duckdb --pre --upgrade", stdout=True, stderr=True)
         print("🦑", container.exec_run("cat /etc/os-release", stdout=True, stderr=True).output.decode())
         print("📌", container.exec_run("python --version", stdout=True, stderr=True).output.decode())
+        container.exec_run("pip install -v duckdb --pre --upgrade", stdout=True, stderr=True)
         result = container.exec_run(
             "python -c \"import duckdb; print(duckdb.sql('SELECT source_id FROM pragma_version()').fetchone()[0])\"",
             stdout=True, stderr=True
