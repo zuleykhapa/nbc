2a72d3304ea0b3f4e5a075c41ebe3123479ff33b
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-01-09 15:43:10 +0100

    scripts/create_names_matrix.py

--- scripts/create_names_matrix.py
+++ scripts/create_names_matrix.py
@@ -2,5 +2,5 @@ import os
 import json
 
 input_files = [f for f in os.listdir() if f.startswith('input') and f.endswith('.json')]
-matrix = [ f'{ file }' for file in input_files]
-print(json.dumps(matrix))
+matrix = [ {"input_file": file } for file in input_files]
+print(json.dumps({"matrix": {"include": matrix_data}}))
