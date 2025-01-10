a093c3102693975f6ed23fcd8a90f3f6564fff54
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-01-10 01:16:50 +0100

    "Windows"

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -42,7 +42,7 @@ def list_all_runs(con):
         "--limit", "50",
         "--json", "status,conclusion,url,name,createdAt,databaseId,headSha",
         "--jq", (
-            '.[] | select(.name == ("LinuxRelease")) '
+            '.[] | select(.name == ("OSX", "LinuxRelease", "Windows")) '
         )
         # the whole list of builds:
         # "--jq", (
