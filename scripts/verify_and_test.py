395ccb41bb431e16b4f24c8974b272c7d102afe1
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-01-20 16:22:37 +0100

    scripts/create_pairs_matrix.py

--- scripts/verify_and_test.py
+++ scripts/verify_and_test.py
@@ -132,14 +132,14 @@ def test_extensions(tested_binary, file_name):
                     print(result.stderr)
                     if result.stderr:
                         if counter == 0:
-                            f.write(f"nightly_build,architecture,runs_on,version,extension,failed_statement\n")
+                            f.write("nightly_build,architecture,runs_on,version,extension,failed_statement\n")
                             counter += 1
                         f.write(f"{ nightly_build },{ architecture },{ runs_on },,{ ext },{ action }\n")
 
                 except subprocess.CalledProcessError as e:
                     with open(file_name, 'a') as f:
                         if counter == 0:
-                            f.write(f"nightly_build,architecture,runs_on,version,extension,failed_statement\n")
+                            f.write("nightly_build,architecture,runs_on,version,extension,failed_statement\n")
                             counter += 1
                         f.write(f"{ nightly_build },{ architecture },{ runs_on },,{ ext },{ action }\n")
                     print(f"Error running command for extesion { ext }: { e }")
