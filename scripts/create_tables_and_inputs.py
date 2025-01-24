c48101050d2e81317627735061d9e3fe5c5f523c
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-01-24 16:42:37 +0100

    .github/workflows/NightlyBuildsCheck.yml

--- scripts/create_tables_and_inputs.py
+++ scripts/create_tables_and_inputs.py
@@ -285,20 +285,20 @@ def main():
         #                 "name": get_binary_name(nightly_build, platform, architecture)
         #             })
 
-    matrix_data.append({
-        "nightly_build": "Python",
-        "architectures": "aarch64",
-        "runs_on": "ubuntu-22.04-arm",
-        "run_id": "12919733489",
-        "name": "aarch64"
-    })
-    matrix_data.append({
-        "nightly_build": "Python",
-        "architectures": "amd64",
-        "runs_on": "ubuntu-latest",
-        "run_id": "12919733489",
-        "name": "amd64"
-    })
+    # matrix_data.append({
+    #     "nightly_build": "Python",
+    #     "architectures": "aarch64",
+    #     "runs_on": "ubuntu-22.04-arm",
+    #     "run_id": "12919733489",
+    #     "name": "aarch64"
+    # })
+    # matrix_data.append({
+    #     "nightly_build": "Python",
+    #     "architectures": "amd64",
+    #     "runs_on": "ubuntu-latest",
+    #     "run_id": "12919733489",
+    #     "name": "amd64"
+    # })
     matrix_data.append({
         "nightly_build": "Python",
         "architectures": "arm64",
@@ -313,13 +313,13 @@ def main():
         "run_id": "12919733489",
         "name": "x86_64"
     })
-    matrix_data.append({
-        "nightly_build": "Python",
-        "architectures": "amd64",
-        "runs_on": "windows-2019",
-        "run_id": "12919733489",
-        "name": "amd64"
-    })
+    # matrix_data.append({
+    #     "nightly_build": "Python",
+    #     "architectures": "amd64",
+    #     "runs_on": "windows-2019",
+    #     "run_id": "12919733489",
+    #     "name": "amd64"
+    # })
 
     with open("inputs.json", "w") as f:
         json.dump(matrix_data, f, indent=4)
