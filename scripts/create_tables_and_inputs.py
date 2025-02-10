b7cef39dd5691b5bd159f7b105d36b0dadc48104
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-02-10 18:21:10 +0100

    hardcode for windows

--- scripts/create_tables_and_inputs.py
+++ scripts/create_tables_and_inputs.py
@@ -292,7 +292,7 @@ def main():
                     "architectures": "amd64",
                     "runs_on": "windows-2019",
                     "run_id": build_info.get('nightly_build_run_id'),
-                    "name": f"windows-{ architecture }"
+                    "name": "windows-amd64"
                 })
             if nightly_build == "LinuxRelease":
                 for architecture in architectures:
