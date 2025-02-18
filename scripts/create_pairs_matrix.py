2efa2b9e40952a6cb25053f052255bde4659750e
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-02-18 15:35:12 +0100

    scripts/create_pairs_matrix.py

--- scripts/create_pairs_matrix.py
+++ scripts/create_pairs_matrix.py
@@ -6,10 +6,13 @@ We would like to run benchmarks comparing
         `current version of the latest release` and (3)
         `current version of the previous release` (4)
 
-The script finds latest and previous versions in current heads (`git ls-remote --heads`) and create last pairs (3), (4) from it.
+The script finds latest and previous versions in current heads (`git ls-remote --heads`) and creates last pairs (3), (4) from it.
 It takes "new_sha" for `main` and for `latest release` in previous version of json file and creates pairs (1), (2).
 It doesn't create pair (2), when there is no "new_sha" for `latest release` in previous version of json file.
 In the end it writes pairs to `duckdb_previous_version_pairs.json` file. The file is used to create a matrix for regression tests run.
+
+If there is a `duckdb_curr_version_main.txt` on the machine, it will create 5th pair `curr-main - old-main` with the
+SHA from the file and removes that file. So the next time it will be creating 4 unique pairs as described above.
 
 Contents of the `duckdb_previous_version_pairs.json` look like this:
 [
@@ -65,8 +68,8 @@ def get_pairs_from_file():
         with open(PAIRS_FILE_PATH, "r") as f:
             data = f.read()
             if len(data):
-                parsed_data = json.loads(data)
-                return parsed_data
+                loaded_data = json.loads(data)
+                return loaded_data
             else:
                 print(f"""
                 `duckdb_previous_version_pairs.json` file found in { PARENT_DIR } but it's empty.
@@ -111,55 +114,65 @@ def parse_branches():
             
     return branches_parsed
 
-def get_pairs():
+def create_pairs_from_old_pairs(old_pairs):
     pairs = []
     parsed_branches = parse_branches()
-    print(parsed_branches)
-    old_pairs = get_pairs_from_file()
-    branches = [br for br in parsed_branches]
-    if old_pairs:
-        for pair in old_pairs:
-            if pair["old_name"] in branches[1:]:
-                new_pair = {
-                    "new_name": branches[0],
-                    "new_sha": parsed_branches[branches[0]],
-                    "old_name": pair["old_name"],
-                    "old_sha": pair["old_sha"]
-                }
-                pairs.append(new_pair)
-            if pair["new_name"] in branches and pair["new_name"] == pair["old_name"]:
-                new_pair = {
-                    "new_name": pair["new_name"],
-                    "new_sha": parsed_branches[pair["new_name"]],
-                    "old_name": pair["new_name"],
-                    "old_sha": pair["new_sha"]
-                }
-                pairs.append(new_pair)
-        unique_pairs = [dict(t) for t in {frozenset(item.items()) for item in pairs}]
-    else:
-        for branch in branches:
+    branch_names = [br for br in parsed_branches]
+    for pair in old_pairs:
+        if pair["old_name"] in branch_names[1:]:
             new_pair = {
-                    "new_name": branches[0],
-                    "new_sha": parsed_branches[branches[0]],
-                    "old_name": branch,
-                    "old_sha": parsed_branches[branch]
-                }
+                "new_name": branch_names[0],
+                "new_sha": parsed_branches[branch_names[0]],
+                "old_name": pair["old_name"],
+                "old_sha": pair["old_sha"]
+            }
             pairs.append(new_pair)
-            if branch in branches[:2]:
-                new_pair = {
-                    "new_name": branch,
-                    "new_sha": parsed_branches[branch],
-                    "old_name": branch,
-                    "old_sha": parsed_branches[branch]
-                }
-                pairs.append(new_pair)
-        unique_pairs = [dict(t) for t in {frozenset(item.items()) for item in pairs}]
+        if pair["new_name"] in branch_names and pair["new_name"] == pair["old_name"]:
+            new_pair = {
+                "new_name": pair["new_name"],
+                "new_sha": parsed_branches[pair["new_name"]],
+                "old_name": pair["new_name"],
+                "old_sha": pair["new_sha"]
+            }
+            pairs.append(new_pair)
+    unique_pairs = [dict(t) for t in {frozenset(item.items()) for item in pairs}]
+    return unique_pairs
+
+def create_pairs_from_branches(old_main = ""):
+    print(create_pairs_from_branches, old_main)
+    pairs = []
+    parsed_branches = parse_branches()
+    branch_names = [br for br in parsed_branches]
+    for branch in branch_names:
+        new_pair = {
+                "new_name": branch_names[0],
+                "new_sha": parsed_branches[branch_names[0]],
+                "old_name": branch,
+                "old_sha": parsed_branches[branch]
+            }
+        pairs.append(new_pair)
+        if branch in branch_names[:2]:
+            new_pair = {
+                "new_name": branch,
+                "new_sha": parsed_branches[branch],
+                "old_name": branch,
+                "old_sha": old_main if (branch == 'main' and old_main) else parsed_branches[branch]
+            }
+            pairs.append(new_pair)
+    unique_pairs = [dict(t) for t in {frozenset(item.items()) for item in pairs}]
     return unique_pairs
 
 def main():
-    pairs = get_pairs()
+    old_txt = maybe_remove_txt_file()
+    old_pairs = get_pairs_from_file()
+    if old_txt:
+        unique_pairs = create_pairs_from_branches(old_txt)
+    elif old_pairs:
+        unique_pairs = create_pairs_from_old_pairs(old_pairs)
+    else:
+        unique_pairs = create_pairs_from_branches()
     with open(PAIRS_FILE_PATH, "w") as f:
-        json.dump(pairs, f, indent=4)
+        json.dump(unique_pairs, f, indent=4)
 
 if __name__ == "__main__":
     main()
