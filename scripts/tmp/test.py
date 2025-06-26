289d2fafee668b0b13e8acb9677bf9e0fbcb62f4
Zuleykha Pavlichenkova <zuleykha.pavlichenkova@gmail.com>
2025-06-26 15:53:46 +0200

    try arnings from the script

--- scripts/tmp/test.py
+++ scripts/tmp/test.py
@@ -1,64 +1,41 @@
-# import datetime
-
-# CURR_DATE = datetime.datetime.now().strftime('%Y-%m-%d')
-# REPORT_FILE = f"{ CURR_DATE }-report.md"
-# print(REPORT_FILE)
-
+import threading
 import argparse
 import json
 
-parser = argparse.ArgumentParser()
-# parser.add_argument("inputs")
-# args = parser.parse_args()
-# loaded_data = args.inputs
+class ErrorContainer:
+    def __init__(self):
+        self._lock = threading.Lock()
+        self._errors = []
 
-# with open(loaded_data, "r") as file:
-#     inputs = json.load(file)
+    def append(self, item):
+        with self._lock:
+            self._errors.append(item)
 
-# for input in inputs:
-#     print("start")
-#     print(input)
-#     print("end")
+    def get_errors(self):
+        with self._lock:
+            return list(self._errors)
 
-# import duckdb
-# con = duckdb.connect()
+    def __len__(self):
+        with self._lock:
+            return len(self._errors)
 
-# con.execute(f"""
-#         CREATE TABLE temp_json AS
-#             SELECT
-#                 *
-#             FROM
-#                 read_json(
-#                     '/Users/zuleykhapavlichenkova/Downloads/leads_stage.json',
-#                     records='auto'
-#                 )
-#             ;
-#     """)
-# result = con.execute("SELECT * FROM temp_json").fetchall()
 
-# for row in result:
-#     print(row)
-
-# import re
-
-# parser.add_argument("file")
-# args = parser.parse_args()
-# file = args.file
-
-# def get_python_versions_from_run(file):
-#     with open(file, "r") as file:
-#         content = file.read()
-#         pattern = r"cp([0-9]+)-.*"
-#         matches = sorted(set(re.findall(pattern, content)))
-#         # puts a '.' after the first character: '310' => '3.10'
-#         result = [word[0] + '.' + word[1:] if len(word) > 1 else word + '.' for word in matches]
-#         print(result)
+error_container = ErrorContainer()
+new_data = {"test": "test/sql/copy/csv/test_skip.test_slow", "return_code": 1, "stdout": "Query unexpectedly failed (test/sql/copy/csv/test_skip.test_slow:193)", "stderr": "FATAL Error: Failed: database has been invalidated because of a previous fatal error. The database must be restarted prior to being used again."}
+error_container.append(new_data)
 
 def main():
-    file_name = "list_failed_ext_{}_{}.md".format("one", "two/three".replace("/", "_"))
-    with open(file_name, 'a') as f:
-        f.write("🦑🦑🦑🦑🦑🦑🦑")
-    # get_python_versions_from_run(file)
+    print(
+    '''\n\n====================================================
+================  FAILURES SUMMARY  ================
+====================================================\n
+'''
+    )
+    for i, error in enumerate(error_container.get_errors(), start=1):
+        print(f"\n{i}:", error["test"], "\n")
+        print(error["stderr"])
+        print(f'echo "::warning::{i}:", {error["test"]}, "\n')
+        print(f'echo "::warning::{error["stderr"]}, "\n')
 
 if __name__ == "__main__":
     main()
