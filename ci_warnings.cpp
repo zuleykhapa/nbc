8c549522d1b178018d4e005c4c40fc04ff07de4e
zuleykhapa <ya-zu@yandex.ru>
2025-07-02 14:11:30 +0200

    change order

--- ci_warnings.cpp
+++ ci_warnings.cpp
@@ -2,14 +2,14 @@
 #include <cstdlib>
 
 int main() {
+    // Example notice message for GitHub Actions
+    std::cout << "::notice::This is a notice message in the CI workflow!" << std::endl;
     // Example warning message for GitHub Actions
     std::cout << "::warning::This is a warning message in the CI workflow!" << std::endl;
 
     // Example error message for GitHub Actions
     std::cerr << "::error::This is an error message in the CI workflow!" << std::endl;
 
-    // Example notice message for GitHub Actions
-    std::cout << "::notice::This is a notice message in the CI workflow!" << std::endl;
 
     return 0;
 }
